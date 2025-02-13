c6fc7e89535e1f37c8f962d91a1ba4b28234cdf9
J. Vandenildo <jvandenildo@gmail.com>
2025-02-13 11:55:25 -0300

    aula 2

---
+++ Aula_02/Gabarito da Aula 2/src/components/Main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ItemList from "./itemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
+
+const Main = () => {
+  return (
+    <div className="main">
+      {/* Lista de artistas */}
+      <ItemList
+        title="Artistas"
+        items={5}
+        itemsArray={artistArray}
+        path="/artists"
+        idPath="/artist"
+      />
+
+      {/* Lista de Músicas */}
+      <ItemList
+        title="Músicas"
+        items={10}
+        itemsArray={songsArray}
+        path="/songs"
+        idPath="/song"
+      />
+    </div>
+  );
+};
+
+export default Main;
